fc825ef0426081e67e0569ab987c8f1d697c0738
tn_mai <rabitfiver@gmail.com>
2018-08-16 18:43:48 +0900

    第07回02章完了時のプロジェクト.

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -1,11 +1,33 @@
 /**
 * @file Main.cpp
 */
+#include "GLFWEW.h"
+#include "Texture.h"
+#include "Sprite.h"
+
+const char windowTitle[] = "OpenGL2D 2018"; // タイトルバーに表示される文章.
+const int windowWidth = 800; // ウィンドウの描画領域の幅.
+const int windowHeight = 600; // ウィンドウの描画領域の高さ.
+
+SpriteRenderer renderer; // スプライトを描画するオブジェクト.
 
 /**
 * プログラムのエントリーポイント.
 */
 int main()
 {
+	// アプリケーションの初期化.
+	GLFWEW::WindowRef window = GLFWEW::Window::Instance();
+	if (!window.Initialize(windowWidth, windowHeight, windowTitle)) {
+		return 1;
+	}
+	if (!Texture::Initialize()) {
+		return 1;
+	}
+	if (!renderer.Initialize(1024)) {
+		return 1;
+	}
+
+	Texture::Finalize();
 	return 0;
 }
