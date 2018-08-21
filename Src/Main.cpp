05fbcdff4ce32bed85cb9fe437aec8047d3dac7f
tn_mai <rabitfiver@gmail.com>
2018-08-21 23:12:34 +0900

    第12回の課題02を実装.

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -39,26 +39,6 @@ Audio::SoundPtr seBlast;
 // ゲームの状態.
 int gamestate; // 実行中の場面ID.
 
-/**
-* タイトル画面用の構造体.
-*
-* タイトル画面で使用する変数などを格納する.
-*/
-struct TitleScene
-{
-	Sprite bg;
-	Sprite logo;
-	const int modeStart = 0; // タイトル表示待ちモード.
-	const int modeTitle = 1; // 入力受付モード.
-	const int modeNextState = 2; // ゲーム開始待ちモード.
-	int mode; // 実行中のモード.
-	float timer; // モード切り替えで使用するタイマー.
-};
-bool initialize(TitleScene*);
-void finalize(TitleScene*);
-void processInput(GLFWEW::WindowRef, TitleScene*);
-void update(GLFWEW::WindowRef, TitleScene*);
-void render(GLFWEW::WindowRef, TitleScene*);
 TitleScene titleScene;
 
 GameOverScene gameOverScene;
