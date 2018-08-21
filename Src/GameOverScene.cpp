3feaba089af8b1dd911949cf2db0c6aa3b3b53a3
tn_mai <rabitfiver@gmail.com>
2018-08-21 19:31:19 +0900

    第12回の課題01を実装.

--- Src/GameOverScene.cpp
+++ Src/GameOverScene.cpp
@@ -2,3 +2,86 @@
 * @file GameOverScene.cpp
 */
 #include "GameOverScene.h"
+#include "GameData.h"
+
+/**
+* ゲームオーバー画面の初期設定を行う.
+*
+* @param scene     ゲームオーバー画面用構造体のポインタ.
+*
+* @retval true  初期化成功.
+* @retval false 初期化失敗.
+*/
+bool initialize(GameOverScene* scene)
+{
+	scene->bg = Sprite("Res/UnknownPlanet.png");
+	scene->timer = 0.5f; // 入力を受け付けない期間(秒).
+	return true;
+}
+
+/**
+* ゲームオーバー画面の終了処理を行う.
+*
+* @param scene  ゲームオーバー画面用構造体のポインタ.
+*/
+void finalize(GameOverScene* scene)
+{
+	scene->bg = Sprite();
+}
+
+/**
+* ゲームオーバー画面のプレイヤー入力を処理する.
+*
+* @param window ゲームを管理するウィンドウ.
+* @param scene  ゲームオーバー画面用構造体のポインタ.
+*/
+void processInput(GLFWEW::WindowRef window, GameOverScene* scene)
+{
+	window.Update();
+	if (scene->timer > 0) {
+		return;
+	}
+	const GamePad gamepad = window.GetGamePad();
+	if (gamepad.buttonDown & (GamePad::A | GamePad::START)) {
+		finalize(scene); // ゲームオーバー画面の後始末.
+		// タイトル画面に戻る.
+		gamestate = gamestateTitle;
+		initialize(&titleScene);
+	}
+}
+
+/**
+* ゲームオーバー画面を更新する.
+*
+* @param window ゲームを管理するウィンドウ.
+* @param scene  ゲームオーバー画面用構造体のポインタ.
+*/
+void update(GLFWEW::WindowRef window, GameOverScene* scene)
+{
+	const float deltaTime = window.DeltaTime();
+	if (scene->timer > 0) {
+		scene->timer -= deltaTime;
+	}
+	scene->bg.Update(deltaTime);
+}
+
+/**
+* ゲームオーバー画面を更新する.
+*
+* @param window ゲームを管理するウィンドウ.
+* @param scene  ゲームオーバー画面用構造体のポインタ.
+*/
+void render(GLFWEW::WindowRef window, GameOverScene* scene)
+{
+	renderer.BeginUpdate();
+	renderer.AddVertices(scene->bg);
+	renderer.EndUpdate();
+	renderer.Draw(glm::vec2(windowWidth, windowHeight));
+
+	fontRenderer.BeginUpdate();
+	fontRenderer.AddString(glm::vec2(-112, 16), "GAME OVER");
+	fontRenderer.EndUpdate();
+	fontRenderer.Draw();
+
+	window.SwapBuffers();
+}
